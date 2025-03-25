0ac28848b417b4f3c8b2a965ec1a182eaa8fd93f
pubestpubest <pubestpubest@gmail.com>
2025-03-25 19:37:00 +0700

    Add CORS middleware and implement file upload functionality

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Book struct {
@@ -14,7 +15,11 @@ var books []Book
 
 func main() {
 	app := fiber.New()
-
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	books = append(books, Book{ID: 1, Title: "Pubest", Author: "Pubest"})
 	books = append(books, Book{2, "Time", "Meka"})
 
@@ -23,6 +28,18 @@ func main() {
 	app.Post("/", createBook)
 	app.Put("/:id", updateBook)
 	app.Delete("/:id", deleteBook)
-
+	app.Post("/upload", uploadFile)
 	app.Listen(":3000")
+}
+
+func uploadFile(c *fiber.Ctx) error {
+	file, err := c.FormFile("image")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	err = c.SaveFile(file, "./upload/"+file.Filename)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	return c.SendString("File upload successfully: " + file.Filename)
 }
