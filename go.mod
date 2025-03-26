a821dd20d286cb2629d17ef889d3aeb1249fc2e2
pubest <110964000+pubestpubest@users.noreply.github.com>
2025-03-26 19:16:52 +0700

    Add JWT authentication support with new login endpoint and update dependencies close Implement simple JWT login #1

--- go.mod
+++ go.mod
@@ -3,20 +3,23 @@ module github.com/pubestpubest/fiber101
 go 1.24.1
 
 require (
-	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/gofiber/contrib/jwt v1.0.10 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/gofiber/template v1.8.3 // indirect
 	github.com/gofiber/template/html/v2 v2.1.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/fasthttp v1.59.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"os"
 	"time"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/template/html/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -49,7 +51,18 @@ func main() {
 	books = append(books, Book{2, "Time", "Meka"})
 
 	app.Post("/login", login)
+	app.Post("/login-jwt", jwtLogin)
+	app.Get("/backdoor", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": os.Getenv("JWT_SECRET"),
+		})
+	})
 	app.Use(logMiddleware)
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{
+			Key: []byte(os.Getenv("JWT_SECRET")),
+		},
+	}))
 	app.Get("/", getBooks)
 	app.Get("/:id", getBook)
 	app.Post("/", createBook)
@@ -65,6 +78,31 @@ func main() {
 	}
 
 	log.Fatal(app.Listen(":" + port))
+}
+
+func jwtLogin(c *fiber.Ctx) error {
+	user := new(User)
+	if err := c.BodyParser(user); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	if user.Email != defaultUser.Email || user.Password != defaultUser.Password {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+	claims := jwt.MapClaims{
+		"email":    user.Email,
+		"password": user.Password,
+		"role":     "admin",
+		"exp":      time.Now().Add(time.Hour * 72).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{
+		"token": t,
+	})
 }
 
 func login(c *fiber.Ctx) error {
